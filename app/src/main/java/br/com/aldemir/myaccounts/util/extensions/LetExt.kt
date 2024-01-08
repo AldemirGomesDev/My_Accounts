d26c45843f8cd7e0b52a08d6f56def4bbaea7d93
Aldemir Gomes da Silva <aldemir.silva@ifood.com.br>
2024-01-08 11:26:43 -0300

    Add extension function safeLet

---
+++ app/src/main/java/br/com/aldemir/myaccounts/util/extensions/LetExt.kt
@@ -0,0 +1,46 @@
+package br.com.aldemir.myaccounts.util.extensions
+
+inline fun <T1, T2, R> safeLet(
+    p1: T1?,
+    p2: T2?,
+    block: (T1, T2) -> R
+): R? {
+    return p1?.let { safeP1 ->
+        p2?.let { safeP2 ->
+            block(safeP1, safeP2)
+        }
+    }
+}
+
+inline fun <T1, T2, T3, R> safeLet(
+    p1: T1?,
+    p2: T2?,
+    p3: T3?,
+    block: (T1, T2, T3) -> R
+): R? {
+    return p1?.let { safeP1 ->
+        p2?.let { safeP2 ->
+            p3?.let { safeP3 ->
+                block(safeP1, safeP2, safeP3)
+            }
+        }
+    }
+}
+
+inline fun <T1, T2, T3, T4, R> safeLet(
+    p1: T1?,
+    p2: T2?,
+    p3: T3?,
+    p4: T4?,
+    block: (T1, T2, T3, T4) -> R
+): R? {
+    return p1?.let { safeP1 ->
+        p2?.let { safeP2 ->
+            p3?.let { safeP3 ->
+                p4?.let { safeP4 ->
+                    block(safeP1, safeP2, safeP3, safeP4)
+                }
+            }
+        }
+    }
+}
