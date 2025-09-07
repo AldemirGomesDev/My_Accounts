2f883af0b14324aba87714e57d75d4a270663749
Aldemir gomes <aldemir.silva@ifood.com.br>
2025-09-07 16:08:53 -0300

    Change ktor version to 3.0.0

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -56,6 +56,19 @@ android {
         }
     }
 
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        debug {
+            isMinifyEnabled = false
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
