fbd694a296b1022a8e98d1459bfb0692688a268c
Aldemir gomes <aldemir.silva@ifood.com.br>
2025-09-08 14:18:05 -0300

    Migrate domain to KMP

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,8 +1,39 @@
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.androidLibrary)
-    alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.kotlinMultiplatform)
 }
 
+kotlin {
+    androidTarget {
+        @OptIn(ExperimentalKotlinGradlePluginApi::class)
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+        }
+    }
+    sourceSets.named("androidMain").configure {
+        kotlin.srcDirs("build/generated/ksp/metada/androidMain/kotlin")
+    }
+
+    sourceSets {
+        androidMain.dependencies {
+            implementation(libs.appCompat)
+            implementation(libs.androidMaterial)
+        }
+        commonMain.dependencies {
+            implementation (libs.bundles.koin.all)
+        }
+        androidInstrumentedTest.dependencies {
+            implementation(libs.espresso.core)
+            implementation(libs.androidx.test.ext.junit)
+        }
+        androidUnitTest.dependencies {
+            implementation(libs.junit)
+        }
+    }
+}
 android {
     namespace = "br.com.aldemir.domain"
     compileSdk = libs.versions.compileSdk.get().toInt()
@@ -15,18 +46,4 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = libs.versions.jvmTarget.get()
-    }
-}
-
-dependencies {
-
-    implementation (libs.bundles.koin.all)
-
-    implementation(libs.appCompat)
-    implementation(libs.androidMaterial)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.espresso.core)
 }
