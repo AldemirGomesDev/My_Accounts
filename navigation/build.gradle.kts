f7e974ced677e48c07924ae10afcb9c12e75ea52
Aldemir gomes <aldemir.silva@ifood.com.br>
2025-09-12 22:48:33 -0300

    Migrate navigation module to kmp

--- navigation/build.gradle.kts
+++ navigation/build.gradle.kts
@@ -1,7 +1,44 @@
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.androidLibrary)
-    alias(libs.plugins.kotlinAndroid)
-    alias(libs.plugins.compose)
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.jetbrainsCompose)
+}
+
+kotlin {
+    androidTarget {
+        @OptIn(ExperimentalKotlinGradlePluginApi::class)
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+        }
+    }
+    sourceSets.named("androidMain").configure {
+        kotlin.srcDirs("build/generated/ksp/metada/androidMain/kotlin")
+    }
+
+    sourceSets {
+        androidMain.dependencies {
+
+        }
+        commonMain.dependencies {
+            implementation(project(":common"))
+            implementation(project(":features:home:impl"))
+            implementation(project(":features:recipe:impl"))
+            implementation(project(":features:expense:impl"))
+            implementation(project(":features:authentication:impl"))
+
+            implementation(compose.material3)
+            implementation(compose.components.resources)
+            implementation(compose.preview)
+            implementation(libs.bundles.compose.all)
+            implementation(libs.compose.lifecycle.viewmodel)
+            implementation(libs.compoose.constraintlayout)
+            implementation(libs.compose.navigation)
+        }
+    }
 }
 
 android {
@@ -33,25 +70,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = libs.versions.jvmTarget.get()
-    }
 }
 
 dependencies {
-
-    implementation(project(":common"))
-    implementation(project(":features:home:impl"))
-    implementation(project(":features:recipe:impl"))
-    implementation(project(":features:expense:impl"))
-    implementation(project(":features:authentication:impl"))
-
-    //Compose
-    implementation(libs.bundles.compose.all)
-    implementation(libs.compose.lifecycle.viewmodel)
-    implementation(libs.compoose.constraintlayout)
-    implementation(libs.compose.material3)
-    implementation(libs.compose.navigation)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
