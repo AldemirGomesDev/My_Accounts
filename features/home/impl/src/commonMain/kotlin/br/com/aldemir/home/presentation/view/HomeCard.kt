6fb706b90c4c5da2c9c07186e8061f2ba5a0a960
Aldemir gomes <aldemir.silva@ifood.com.br>
2025-09-17 00:19:12 -0300

    Refactor HomeCard to use generated resource identifiers for strings and drawables

--- features/home/impl/src/commonMain/kotlin/br/com/aldemir/home/presentation/view/HomeCard.kt
+++ features/home/impl/src/commonMain/kotlin/br/com/aldemir/home/presentation/view/HomeCard.kt
@@ -9,17 +9,23 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.text.style.TextAlign
 import br.com.aldemir.home.presentation.model.HomeCardData
 import br.com.aldemir.common.theme.*
-import br.com.aldemir.myaccounts.common.R
 import br.com.aldemir.common.theme.MyAccountsTheme.MyAccountsTheme
 import br.com.aldemir.common.util.emptyString
 import br.com.aldemir.common.util.getCurrencySymbol
 import br.com.aldemir.common.util.toCurrency
+import myaccounts.common.generated.resources.Res
+import myaccounts.common.generated.resources.account_resume_monthly
+import myaccounts.common.generated.resources.home_add_expense
+import myaccounts.common.generated.resources.home_balance
+import myaccounts.common.generated.resources.home_recipe_card_title
+import myaccounts.common.generated.resources.ic_check_circle
+import myaccounts.common.generated.resources.ic_report_problem
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun HomeCard(
@@ -40,7 +46,7 @@ fun HomeCard(
             Text(
                 modifier = Modifier.fillMaxWidth()
                     .padding(bottom = MyAccountsTheme.dimensions.padding8),
-                text = stringResource(id = R.string.account_resume_monthly),
+                text = stringResource(Res.string.account_resume_monthly),
                 color = Color.White,
                 style = MyAccountsTheme.typography.subTitleBold,
                 textAlign = TextAlign.Center
@@ -55,14 +61,14 @@ fun HomeCard(
                 ) {
                     Image(
                         modifier = Modifier,
-                        painter = painterResource(id = R.drawable.ic_check_circle),
+                        painter = painterResource(Res.drawable.ic_check_circle),
                         contentDescription = emptyString()
                     )
                     Column(modifier = Modifier.padding(start = MyAccountsTheme.dimensions.padding8)) {
                         Text(
                             modifier = Modifier
                                 .padding(start = MyAccountsTheme.dimensions.padding4),
-                            text = stringResource(id = R.string.recipe_card_title),
+                            text = stringResource(Res.string.home_recipe_card_title),
                             color = Color.White,
                             style = MyAccountsTheme.typography.paragraph02Bold
                         )
@@ -83,7 +89,7 @@ fun HomeCard(
                     Image(
                         modifier = Modifier
                             .padding(start = MyAccountsTheme.dimensions.padding24),
-                        painter = painterResource(id = R.drawable.ic_report_problem),
+                        painter = painterResource(Res.drawable.ic_report_problem),
                         contentDescription = emptyString(),
                         colorFilter = ColorFilter.tint(color = MediumPriorityColor)
                     )
@@ -96,7 +102,7 @@ fun HomeCard(
                                 .padding(
                                     start = MyAccountsTheme.dimensions.padding4,
                                 ),
-                            text = stringResource(id = R.string.add_expense),
+                            text = stringResource( Res.string.home_add_expense),
                             color = Color.White,
                             style = MyAccountsTheme.typography.paragraph02Bold
                         )
@@ -120,7 +126,7 @@ fun HomeCard(
                 Text(
                     modifier = Modifier
                         .padding(start = MyAccountsTheme.dimensions.padding4),
-                    text = stringResource(id = R.string.home_balance),
+                    text = stringResource(Res.string.home_balance),
                     color = Color.White,
                     style = MyAccountsTheme.typography.paragraph02Bold
                 )
