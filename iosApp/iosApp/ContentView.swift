bb83b50259f82eb1cb166e8841d8879f8c342cb8
Aldemir gomes <aldemir.silva@ifood.com.br>
2025-09-20 22:51:01 -0300

    Update ContentView to ignore safe area for keyboard

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -13,7 +13,7 @@ struct ComposeView: UIViewControllerRepresentable {
 struct ContentView: View {
     var body: some View {
         ComposeView()
-            .ignoresSafeArea()
+            .ignoresSafeArea(.keyboard)
     }
 }
 
